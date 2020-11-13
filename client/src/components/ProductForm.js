9e2f58c1dfea6c8819c27687f8a0adb145485721
Daniel Nguyen <dung.nguyen@jessup.edu>
2020-11-13 12:51:31 -0800

    first commit

---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+export default () => {
+  //keep track of what is being typed via useState hook
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  //handler when the form is submitted
+  const onSubmitHandler = (e) => {
+    //prevent default behavior of the submit
+    e.preventDefault();
+    //make a post request to create a new person
+    axios
+      .post("http://localhost:8000/api/product", {
+        title,
+        price,
+        description,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+  //onChange to update firstName and lastName
+  return (
+    <div class="form-style-6">
+      <h1>Product Manager</h1>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <input
+          type="text"
+          name="price"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
